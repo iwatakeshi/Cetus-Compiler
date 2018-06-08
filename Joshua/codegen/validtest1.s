1a93def677adb87506865e9959395be8568ca2fd
Marcus Griffiths <marcusgriffiths@Marcuss-MBP.attlocal.net>
2018-06-08 11:05:56 -0700

    preparing to pull

---
+++ Joshua/codegen/validtest1.s
@@ -0,0 +1,32 @@
+.text
+
+.globl Main
+Main:
+	pushl %ebp
+	movl %esp, %ebp
+	pushl %ebx
+	pushl %esi
+	pushl %edi
+	pushl $0
+	popl %eax
+	popl %edi
+	popl %esi
+	popl %ebx
+	leave
+	ret
+.text
+
+.globl Main
+Main:
+	pushl %ebp
+	movl %esp, %ebp
+	pushl %ebx
+	pushl %esi
+	pushl %edi
+	pushl $0
+	popl %eax
+	popl %edi
+	popl %esi
+	popl %ebx
+	leave
+	ret
